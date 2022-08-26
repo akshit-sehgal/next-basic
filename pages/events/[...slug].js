0392f5631632598bdb2538ebbcd8d37f06c34f7a
akshit-sehgal <akshitsehgal95@gmail.com>
2022-08-26 20:34:22 +0530

    feat: adding client side fetching instead of SSR for slug

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,14 +1,64 @@
-import React, { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import React, { Fragment, useEffect, useState } from 'react';
+import useSWR from 'swr';
 import EventList from '../../components/events/EventList';
 import ResultsTitle from '../../components/events/ResultsTitle';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/ErrorAlert';
 import { getFilteredEvents } from '../../helpers/apiUtils';
 
-const FilteredEventsPage = (props) => {
-  const { events, numYear, numMonth, hasError } = props;
+const fetcher = (url)=>fetch(url).then((resp)=>resp.json());
 
-  if (hasError) {
+const FilteredEventsPage = () => {
+
+  const { query: { slug: filterData } } = useRouter();
+
+  const [events, setEvents] = useState();
+  
+  const { data, error } = useSWR('https://next-temp.firebaseio.com/events.json', fetcher);
+
+  useEffect(() => {
+    if (data) {
+      const events = [];
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key]
+        })
+      }
+      setEvents(events);
+    }
+  }, [data]);
+
+  if (!events) {
+    return (
+      <p className='center'>Loading...</p>
+    );
+  }
+
+  const [year, month] = filterData;
+  const numYear = +year;
+  const numMonth = +month;
+
+  if (isNaN(numYear) || isNaN(numMonth) || numYear > 2022 || numYear < 2021 || numMonth > 12 || numMonth < 1 || error) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filters</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = events.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+  });
+
+  if (!filteredEvents.length) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -26,46 +76,9 @@ const FilteredEventsPage = (props) => {
   return (
     <Fragment>
       <ResultsTitle date={filterDate} />
-      <EventList items={events} />
+      <EventList items={filteredEvents} />
     </Fragment>
   );
-};
-
-export const getServerSideProps = async (ctx) => {
-  const { params } = ctx;
-
-  const filterData = params.slug;
-
-  const [year, month] = filterData;
-  const numYear = +year;
-  const numMonth = +month;
-  if (isNaN(numYear) || isNaN(numMonth) || numYear > 2022 || numYear < 2021 || numMonth > 12 || numMonth < 1) {
-    return {
-      props:{
-        hasError: true
-      }
-    }
-  }
-
-  const events = await getFilteredEvents({
-    year: numYear,
-    month: numMonth
-  });
-
-  if (!events || !events.length) {
-    return {
-      props:{
-        hasError: true
-      }
-    }
-  }
-  return {
-    props: {
-      events,
-      numYear,
-      numMonth
-    }
-  };
 };
 
 export default FilteredEventsPage;
